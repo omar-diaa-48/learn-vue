b5565d7d03d05eaf46f2930d7cc412816e7e3504
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-07-29 13:18:17 +0200

    add modal

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
-  <div class="mt-8 flex flex-col items-center">
+  <div class="flex flex-col items-center">
     <h1>{{ title }}</h1>
-    <input class="border rounded" type="text" ref="name" />
-    <button @click="handleClick">Click me</button>
+    <Modal
+      v-if="showModal"
+      :header="header"
+      :text="text"
+      theme="sale"
+      @close="toggleModal"
+    />
+    <button @click="toggleModal">Open Modal</button>
   </div>
 </template>
 
+
 <script>
+import Modal from "./components/Modal.vue";
+
 export default {
   name: "App",
   data() {
     return {
       title: "My First Vue App :)",
+      header: "Sign up",
+      text: "Grab your courses now!",
+      showModal: false,
     };
   },
   methods: {
-    handleClick() {
-      this.$refs.name.focus();
+    toggleModal() {
+      this.showModal = !this.showModal;
     },
+  },
+  components: {
+    Modal,
   },
 };
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    @click.self="closeModal"
+    class="top-0 fixed bg-slate-500 bg-opacity-50 w-full h-full"
+  >
+    <div class="w-96 p-5 mx-auto my-24 bg-white rounded-lg">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
